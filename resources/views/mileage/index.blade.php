4dbca0f79c03e9be32328d464d7dc4018fee8755
Steven Woolston <steven.woolston@gmail.com>
2020-12-26 00:17:51 +1000

    WIP

--- resources/views/mileage/index.blade.php
+++ resources/views/mileage/index.blade.php
@@ -22,7 +22,20 @@
             </div>
             <div class="form-group">
                 <label for="odometer">Odometer</label>
-                <input type="number" id="odometer" class="form-control" id="odometer">
+                <div class="radio">
+                    <label for="kms">
+                        <input type="radio" id="kms" name="distance" checked>
+                        Kilometres traveled
+                        <input type="number" id="kms_value" class="form-control" />
+                    </label>
+                </div>
+                <div class="radio">
+                    <label for="odometer">
+                        <input type="radio" id="odometer" name="distance">
+                        Odometer reading
+                        <input type="number" id="odometer_value" class="form-control" />
+                    </label>
+                </div>
             </div>
             <div class="form-group">
                 <label for="">Date</label>
@@ -32,6 +45,17 @@
                 <button type="button" class="form-control btn btn-primary">Save</button>
             </div>
         </form>
+
+        <table id="mileage" class="table">
+            <colgroup>
+                <col style="width: 50%" />
+                <col style="width: 50%" />
+            </colgroup>
+            <thead>
+                <tr><td>Vehicle</td><td>Distance Traveled</td></tr>
+            </thead>
+            <tbody></tbody>
+        </table>
     </div><!-- /page-wrap -->
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -42,16 +66,39 @@
                 saveMileage($("#vehicle"), $("#odometer"), $("#date_of_travel"));
             });
 
+            $("select#vehicle").on("change", function(e) {
+                getMileage($(this).val());
+            });
+
             fetch('http://127.0.0.1:8000/api/odometer/vehicles', {
                 method: "get"
             }).then(function(response) {
                 return response.json();
             }).then(function(data) {
                 data.data.forEach(function(item) {
-                    $("#vehicle").append(`<option value="${item.id}">${item.name}</option>`);
+                    $("select#vehicle").append(`<option value="${item.id}">${item.name}</option>`);
                 })
+            })
+            .then(function(data) {
+                getMileage($("select#vehicle").val());
             });
         });
+
+        function getMileage(vehicleId) {
+            fetch(`http://127.0.0.1:8000/api/odometer/vehicle/${vehicleId}/odometers`, {
+                method: 'get',
+                headers: {"Content-type": "application/json; charset=UTF-8"},
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                var $mileage = $("table#mileage tbody");
+                data.data.forEach(function(item) {
+                    $mileage.append(`<tr><td>${item.vehicle.name}</td><td>${item.distance_traveled}</td></tr>`);
+                })
+            });
+        }
 
         function saveMileage(vehicle, odometer, travelDate) {
             var vehicleId = vehicle.val();
@@ -66,6 +113,12 @@
                     "is_kms": true,
                     "date_of_travel": travelDate.val()
                 })
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                getMileage(data.vehicle_id);
             });
         }
     </script>
